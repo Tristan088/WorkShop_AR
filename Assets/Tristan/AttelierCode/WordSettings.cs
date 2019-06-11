5127206882c5ae64de53f19d687c5e59e6ed7fd0
Ramirez De Ribas Tristan <tristan.ramirez-de-ribas@e-artsup.net>
2019-06-11 12:34:57 +0200

    Classe Perso + Singleton WorldSetting

---
+++ Assets/Tristan/AttelierCode/WordSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName ="Settings/World")]
+public class WordSettings : SingletonSettings<WordSettings>
+{
+    public Perso player;
+
+   
+	
+}
---
+++ Assets/Tristan/Script/Perso.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perso : MonoBehaviour
+{
+
+
+    public int hpMax;
+    public int hpCurrent;
+    public int[] stats = new int[4];
+    public int[] carac = new int[20];
+    public int[] caracMod = new int[20];
+    public string nom;
+    public string prenom;
+    public string surnom;
+    public int lvl;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
