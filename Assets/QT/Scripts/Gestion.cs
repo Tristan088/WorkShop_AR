5ef69a323b49968585cb5c1ddbdd1cc6d849aae0
SALA QuangTu <sala.quangtu@gmail.com>
2019-06-11 13:59:39 +0200

    scene + script gestion

---
+++ Assets/QT/Scripts/Gestion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gestion : MonoBehaviour
+{
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ThrowDice(string diceValue)
+    {
+
+    }
+}
