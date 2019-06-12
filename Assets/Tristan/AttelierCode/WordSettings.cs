116e52354e80f0dd8abecb75c26931496a0dc6c5
Ramirez De Ribas Tristan <tristan.ramirez-de-ribas@e-artsup.net>
2019-06-12 11:40:59 +0200

    Debut de creation de perso

--- Assets/Tristan/AttelierCode/WordSettings.cs
+++ Assets/Tristan/AttelierCode/WordSettings.cs
@@ -6,8 +6,45 @@ using UnityEngine;
 [CreateAssetMenu(menuName ="Settings/World")]
 public class WordSettings : SingletonSettings<WordSettings>
 {
-    public Perso player;
+    
+    public int hpMax;
+    public int hpCurrent;
+    public int[] stats = new int[4];
+    public int[] statsMod = new int[4];
+    public int[] carac = new int[20];
+    public int[] caracMod = new int[20];
+    public string nom;
+    public string prenom;
+    public string surnom;
+    public int lvl;
 
-   
-	
+
+    public bool PlayerExist()
+    {
+        return nom != null;
+    }
+
+    public void ResetPlayer()
+    {
+        hpMax = 0;
+        hpCurrent = 0;
+        for (int i = 0; i<4; i++)
+        {
+            stats[i] = 10;
+            statsMod[i] = -4;
+        }
+
+        for(int i = 0; i<20; i++)
+        {
+            carac[i] = 10;
+            caracMod[i] = 0;
+        }
+
+
+        nom = prenom = surnom = null;
+
+        lvl = 1;
+        
+    }
 }
+
---
+++ Assets/Tristan/Script/UIManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    private int lvl;
+
+    public Text lvlText;
+    public Text nom;
+    public Text prenom;
+    public Text surnom;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lvl = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LvlPlusUn()
+    {
+        
+        lvl++;
+        if (lvl > 30)
+        {
+            lvl = 30;
+        }
+        lvlText.text = lvl.ToString();
+    }
+
+    public void LvlMoinUn()
+    {
+        
+        lvl--;
+        if (lvl < 1)
+        {
+            lvl = 1;
+        }
+        lvlText.text = lvl.ToString();
+    }
+
+    public void ValidateNom()
+    {
+        WordSettings.Instance.prenom = prenom.text;
+        WordSettings.Instance.nom = nom.text;
+        WordSettings.Instance.surnom = surnom.text;
+        WordSettings.Instance.lvl = lvl;
+
+    }
+
+
+
+
+}
