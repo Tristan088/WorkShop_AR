9567b9d15617c405d65ec33872f54eaa20964677
Ramirez De Ribas Tristan <tristan.ramirez-de-ribas@e-artsup.net>
2019-06-17 15:10:05 +0200

    offset normalement

---
+++ Assets/Tristan/Script/WaterOffsetMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterOffsetMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Material _material;
+    private float offset = 0;
+    void Start()
+    {
+       _material = gameObject.GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset += Time.deltaTime;
+        _material.SetTextureOffset("_MainTex", new Vector2(offset, 0f));
+    }
+}
