8cde0868ac389205a6bec5be790fc674fb431131
Ramirez De Ribas Tristan <tristan.ramirez-de-ribas@e-artsup.net>
2019-06-18 11:04:18 +0200

    Offeset eau + fin de la création perso

--- Assets/Tristan/Script/WaterOffsetMove.cs
+++ Assets/Tristan/Script/WaterOffsetMove.cs
@@ -7,6 +7,7 @@ public class WaterOffsetMove : MonoBehaviour
     // Start is called before the first frame update
     Material _material;
     private float offset = 0;
+    private float offset2 = 0;
     void Start()
     {
        _material = gameObject.GetComponent<Renderer>().material;
@@ -15,7 +16,9 @@ public class WaterOffsetMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        offset += Time.deltaTime;
-        _material.SetTextureOffset("_MainTex", new Vector2(offset, 0f));
+        offset += Time.deltaTime/2;
+        offset2 += Time.deltaTime/3;
+        _material.SetTextureOffset("_DetailAlbedoMap", new Vector2(offset, 0f));
+        _material.SetTextureOffset("_MainTex", new Vector2(-offset2, 0f));
     }
 }
