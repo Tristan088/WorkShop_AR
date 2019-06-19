bb86903d9093fa69f766c38acdaab1ad7d7cee03
Heaxys <mondhorchihi@aol.fr>
2019-06-19 12:22:50 +0200

    Le script du lancer de dé

---
+++ Assets/Mondhor/Lancer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lancer : MonoBehaviour
+{
+    public Transform dé;
+    public bool locked = false;
+
+    private void Start()
+    {
+            GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && locked == false)
+        {
+            GetComponent<Rigidbody>().AddForce(dé.forward * Random.Range(900, 1100));
+            Debug.Log("ça applique une force");
+            locked = true;
+            GetComponent<Rigidbody>().useGravity = true;
+        }
+
+        if (locked == true)
+        {
+            GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+}
